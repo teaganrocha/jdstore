6ed6275bea767f1d238eeeef3be6b98ab158390a
bigpoet <bigpoet@gmail.com>
2017-02-07 23:09:07 +0800

    update admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,6 @@ u.save
 
 create_account = User.create([email: 'test@gmail.com', password:'123456', password_confirmation: '123456', is_admin: 'true'])
 puts "Admin account created."
+
+create_account = User.create([email: 'bigpoet@gmail.com', password:'qwe81320', password_confirmation: 'qwe81320', is_admin: 'true'])
+puts "Admin account created."
