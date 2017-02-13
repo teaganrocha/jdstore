e423862c7c6ee904e40bc492fc867596387520cb
bigpoet <bigpoet@gmail.com>
2017-02-13 22:54:08 +0800

    add payment_method fuction

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
  end
 
  resources :cart_items
- resources :orders
+
+ resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
  resources :products do
      member do
